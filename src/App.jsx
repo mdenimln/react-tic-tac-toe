cd66d35a1808953d2677d7bbef7482b51a2d8241
Deni Maulana <denimlnm@gmail.com>
2023-06-24 03:49:23 +0700

    app tic tac toe v:1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GameBoard/index.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+// import React from 'react'
+import './style.css'
+
+// eslint-disable-next-line react/prop-types
+const GameBoard = ({ winner,currentPlayer,winningCells,handleCellClick,resetGame,board }) => {
+    return (
+        <div className='game-board'>
+            {winner ? winner === "draw" ? (
+                <p className='winner-title'>Its a draw!</p>
+            ) : (
+                <p className='winner-title'>Player
+                    <span className={winner}> {winner}</span> wins!
+                </p>
+            ) : <p className={`winner-title`}>Current Player:
+                <span className={currentPlayer}> {currentPlayer}</span>
+            </p>}
+            <div className="board">
+                {board.map((cell, index) => (
+                    <div
+                        key={index}
+                        className={`cell ${winningCells.includes(index) ?'winning':''} ${cell}`}
+                        onClick={() => handleCellClick(index)}
+                    >
+                        {cell}
+                    </div>
+                ))}
+            </div>
+            {winner && (
+                <div className="message">
+                    <button onClick={resetGame}>Play Again</button>
+                </div>
+            )}</div>
+    )
+}
+
+export default GameBoard
